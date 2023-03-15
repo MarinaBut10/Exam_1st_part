f66eff90c560cb22832dc31721b346da6164f82a
Marina <lemur4icc@mail.ru>
2023-03-15 13:03:01 +0300

    New folder added

---
+++ Задача/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая из имеющегося массива строк формирует массив строк, 
+// длина которых меньше либо равна 3 символа
+// Первоначальный массив можно ввести с клавиатурыы, либо задать на старте выполнения алгоритма.
+
+Console.Clear();
+
+string[] Array(string[] array)
+{
+    int index = 0;
+    string[] rezult = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            rezult[index] = array[i];
+            index++;
+        }
+    }
+    return rezult;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] Getarray = { "<3", "wish", "you", "best" };
+string[] newArray = Array(Getarray);
+PrintArray(newArray);
